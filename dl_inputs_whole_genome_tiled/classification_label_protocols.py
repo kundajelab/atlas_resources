bc0247c35961106dba7f8ce8452e5bff8c54eccc
anna shcherbina <annashcherbina@gmail.com>
2018-12-13 01:38:55 -0800

    debugged

--- dl_inputs_whole_genome_tiled/classification_label_protocols.py
+++ dl_inputs_whole_genome_tiled/classification_label_protocols.py
@@ -1,5 +1,5 @@
-from math import floor
-
+from math import floor,ceil
+import pdb 
 def label_ambiguous_bins(chrom,task_name,non_zero_bins,min_bin_start,max_bin_start,args):
     left_ambiguous_start= min_bin_start-args.stride
     left_ambiguous_end=left_ambiguous_start+args.bin_size
@@ -18,7 +18,7 @@ def label_ambiguous_bins(chrom,task_name,non_zero_bins,min_bin_start,max_bin_sta
     non_zero_bins[right_ambiguous_bin][task_name]='-1'
     return non_zero_bins
 
-def peak_summit_near_bin_center(task_name,task_bed,args,non_zero_bins):
+def peak_summit_near_bin_center(task_name,task_bed,args):
     '''
     For each peak, the summit position is determined. 
 
@@ -30,24 +30,22 @@ def peak_summit_near_bin_center(task_name,task_bed,args,non_zero_bins):
     If specified in args.allow_ambiguous, then adjacent bins to the two extremes are marked as 
     ambiguous 
     '''
+    non_zero_bins=dict() 
     for entry in task_bed:
         chrom=entry[0]
-        peak_start=entry[1]
-        summit=peak_start+entry[-1]
-        
-        min_bin_center_start=floor((summit-args.bin_center_size)/args.stride)*args.stride
+        peak_start=int(entry[1])
+        summit=peak_start+int(entry[-1])
+        min_bin_center_start=ceil((summit-args.bin_center_size)/args.stride)*args.stride
         max_bin_center_start=floor(summit/args.stride)*args.stride 
 
         #padding to the left and right of the bin_center_size region needed to generated the desired bin_size 
         topad=int(round((args.bin_size-args.bin_center_size)/2))
-
         #label all bins with center regions between min_bin_center_start and max_bin_center_start (inclusive) with 1 
-        for(bin_center_start in range(min_bin_center_start,max_bin_center_start+1,args.stride)):
+        for bin_center_start in range(min_bin_center_start,max_bin_center_start+1,args.stride):
             cur_bin_start=bin_center_start-topad
-            cur_bin_end=bin_center_start+args.bin_size 
+            cur_bin_end=cur_bin_start+args.bin_size 
             cur_bin=(chrom,cur_bin_start,cur_bin_end)
-            if cur_bin not in non_zero_bins:
-                non_zero_bins[cur_bin]=dict()
+            non_zero_bins[cur_bin]=dict()
             non_zero_bins[cur_bin][task_name]='1'
 
         #if user specified use of ambiguous bins,
@@ -59,11 +57,12 @@ def peak_summit_near_bin_center(task_name,task_bed,args,non_zero_bins):
             non_zero_bins=label_ambiguous_bins(chrom,task_name,non_zero_bins,min_bin_start,max_bin_start,args)
     return non_zero_bins
 
-def peak_percent_overlap_with_bin(task_name,task_bed,args,non_zero_bins):
+def peak_percent_overlap_with_bin(task_name,task_bed,args):
+    non_zero_bins=dict()
     for entry in task_bed:
         chrom=entry[0]
-        peak_start=entry[1]
-        peak_end=entry[2]
+        peak_start=int(entry[1])
+        peak_end=int(entry[2])
 
         peak_length=peak_end-peak_start+1
         min_overlap=min([peak_length,args.overlap_thresh])/2
@@ -71,10 +70,9 @@ def peak_percent_overlap_with_bin(task_name,task_bed,args,non_zero_bins):
         overlap_bin_first_start=(floor(peak_start-min_overlap)/args.stride)*args.stride
         overlap_bin_last_start=peak_end+min_overlap-args.bin_size
 
-        for(bin_start in range(overlap_bin_first_start,overlap_bin_last_start+1,args.stride):
+        for bin_start in range(overlap_bin_first_start,overlap_bin_last_start+1,args.stride ):
             cur_bin=(chrom,bin_start,bin_start+args.bin_size)
-            if cur_bin not in non_zero_bins:
-                non_zero_bins[cur_bin]=dict()
+            non_zero_bins[cur_bin]=dict()
             non_zero_bins[cur_bin][task_name]='1'
 
         if(args.allow_ambiguous==True): 
--- dl_inputs_whole_genome_tiled/generate_inputs_tiled_whole_genome_indexed.sh
+++ dl_inputs_whole_genome_tiled/generate_inputs_tiled_whole_genome_indexed.sh
@@ -1,6 +1,6 @@
 python generate_inputs_tiled_whole_genome_indexed.py --task_list tasks.tsv \
        --out_bed labels.tsv \
-       --chrom_sizes hg38.chrom.sizes \
+       --chrom_sizes hg38.chrom21.sizes \
        --stride 50 \
        --bin_size 1000 \
        --bin_center_size 200 \
