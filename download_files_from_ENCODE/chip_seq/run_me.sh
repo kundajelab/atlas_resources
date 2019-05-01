0e53a54863318e4fccb331948dfc55b4142878bb
AvantiShri <avanti.shrikumar@gmail.com>
2019-05-01 14:42:47 -0700

    updated runme to remove metadata from filenames

--- download_files_from_ENCODE/chip_seq/run_me.sh
+++ download_files_from_ENCODE/chip_seq/run_me.sh
@@ -9,7 +9,7 @@
 curl -L "https://www.encodeproject.org/report.tsv?type=File&output_type=optimal+idr+thresholded+peaks&lab.title=ENCODE+Processing+Pipeline&status=released&assembly=GRCh38&file_format=bed&field=%40id&field=dataset&field=technical_replicates&field=biological_replicates&field=href&limit=all" > metadata_optimalidr_encodeprocessed_report.tsv
 ./augment_info.py
 
-cat augmented_metadata_optimalidr_encodeprocessed_report.tsv | perl -lane 'BEGIN {use File::Basename} if ($. > 1) {print "wget https://www.encodeproject.org/".$F[4]." -O ".$F[5]."_".$F[6]."_".basename($F[4])}' > download_script.sh
+cat metadata_optimalidr_encodeprocessed_report.tsv | perl -lane 'BEGIN {use File::Basename} if ($. > 2) {print "wget https://www.encodeproject.org/".$F[4]." -O ".basename($F[4])}' > download_script.sh
 
 mkdir data
 cd data
