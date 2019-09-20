e66fcb0daa55aa17cf4177d46babef9c0eb0e887
Av Shrikumar <avanti.shrikumar@gmail.com>
2019-09-20 12:54:32 -0700

    Update pull_metadata.sh

--- download_files_from_ENCODE/chip_seq_with_relaxed_peaks/pull_metadata.sh
+++ download_files_from_ENCODE/chip_seq_with_relaxed_peaks/pull_metadata.sh
@@ -11,6 +11,8 @@
 
 #Query has been updated to have case sensitivity...used to be "idr", but now has to be IDR
 # Good idea to manually inspect to make sure query is working as expected since apparently the API changes from time to time.
+#Also rather than using complement-type syntax to specify the desired files, going forward it may be better to list out the desired file types, which is
+# achieved by the following query: https://www.encodeproject.org/report/?type=File&lab.title=ENCODE+Processing+Pipeline&status=released&assembly=GRCh38&file_format=bed&output_type=peaks+and+background+as+input+for+IDR&output_type=optimal+IDR+thresholded+peaks&field=%40id&field=accession&field=dataset&field=assembly&field=output_type&field=technical_replicates&field=biological_replicates&field=href
 curl -L "https://www.encodeproject.org/report.tsv?type=File&lab.title=ENCODE+Processing+Pipeline&status=released&assembly=GRCh38&field=%40id&field=accession&field=dataset&field=assembly&field=output_type&field=technical_replicates&field=biological_replicates&field=href&file_format=bed&output_type%21=peaks&output_type%21=conservative+IDR+thresholded+peaks&output_type%21=replicated+peaks&output_type%21=sequence+alignability&output_type%21=methylation+state+at+CpG&output_type%21=methylation+state+at+CHH&output_type%21=methylation+state+at+CHG&output_type%21=enrichment&output_type%21=hotspots&output_type%21=pseudoreplicated+IDR+thresholded+peaks&output_type%21=stable+peaks&output_type%21=transcription+start+sites" > unordered_metadata_optimalandrelaxedpeaks_encodeprocessed.temp
 
 #Go through a sorting step to make the order deterministic (it's not deterministic when pulled from web)
