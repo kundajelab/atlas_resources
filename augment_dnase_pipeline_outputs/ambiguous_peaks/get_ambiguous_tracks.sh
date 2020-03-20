518551812b665a55db563669190895a63f80d084
anna shcherbina <annashcherbina@gmail.com>
2020-03-20 10:33:57 -0700

    updated ambiguous peak sets with the new blacklists

--- augment_dnase_pipeline_outputs/ambiguous_peaks/get_ambiguous_tracks.sh
+++ augment_dnase_pipeline_outputs/ambiguous_peaks/get_ambiguous_tracks.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 idr_peaks=$1
 overlap_peaks=$2
-/share/PI/euan/apps/bin/bedtools intersect -v -a $overlap_peaks -b $idr_peaks| bgzip -c  > $overlap_peaks.ambiguous.bed.gz 
+bedtools intersect -v -a $overlap_peaks -b $idr_peaks | cut -f1,2,3 > $overlap_peaks.ambiguous.bed
+cut -f1,2,3 ../../make_pygenometracks/all_three_blacklists.bed >> $overlap_peaks.ambiguous.bed
+bedtools sort -i $overlap_peaks.ambiguous.bed > $overlap_peaks.ambiguous.bed.tmp
+mv $overlap_peaks.ambiguous.bed.tmp $overlap_peaks.ambiguous.bed
+bgzip -f $overlap_peaks.ambiguous.bed
+
