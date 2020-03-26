f5850550797a333fafc68fc04cbc5ac2fb535176
Anna Shcherbina <annashcherbina@gmail.com>
2020-03-26 15:21:34 -0700

    add ambiguous from 3 blacklists

--- augment_dnase_pipeline_outputs/ambiguous_peaks/get_ambiguous_tracks.sh
+++ augment_dnase_pipeline_outputs/ambiguous_peaks/get_ambiguous_tracks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 idr_peaks=$1
 overlap_peaks=$2
-bedtools intersect -v -a $overlap_peaks -b $idr_peaks | cut -f1,2,3 > $overlap_peaks.ambiguous.bed
-cut -f1,2,3 ../../make_pygenometracks/all_three_blacklists.bed >> $overlap_peaks.ambiguous.bed
-bedtools sort -i $overlap_peaks.ambiguous.bed > $overlap_peaks.ambiguous.bed.tmp
-mv $overlap_peaks.ambiguous.bed.tmp $overlap_peaks.ambiguous.bed
+#bedtools intersect -v -a $overlap_peaks -b $idr_peaks | cut -f1,2,3 > $overlap_peaks.ambiguous.bed
+#cut -f1,2,3 ../../make_pygenometracks/all_three_blacklists.bed >> $overlap_peaks.ambiguous.bed
+#bedtools sort -i $overlap_peaks.ambiguous.bed > $overlap_peaks.ambiguous.bed.tmp
+#mv $overlap_peaks.ambiguous.bed.tmp $overlap_peaks.ambiguous.bed
 bgzip -f $overlap_peaks.ambiguous.bed
 
--- augment_dnase_pipeline_outputs/ambiguous_peaks/sbatch_get_ambiguous_tracks.sh
+++ augment_dnase_pipeline_outputs/ambiguous_peaks/sbatch_get_ambiguous_tracks.sh
@@ -4,7 +4,7 @@
 idr_peak_files=idr.optimal.narrowPeak
 overlap_peak_files=overlap.optimal.narrowPeak
 numlines=`cat $idr_peak_files |wc -l`
-for i in 1 `seq 1 $numlines`
+for i in `seq 1 $numlines`
 do
     idr_peaks=`head -n$i $idr_peak_files | tail -n1`
     overlap_peaks=`head -n$i $overlap_peak_files | tail -n1`
