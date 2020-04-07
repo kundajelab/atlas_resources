06f75d1e221e020186ded19d08815249bb7ccba0
Anna Shcherbina <annashcherbina@gmail.com>
2020-04-07 14:33:33 -0700

    updated ambiguous peak scripts

--- augment_dnase_pipeline_outputs/ambiguous_peaks/get_ambiguous_tracks.sh
+++ augment_dnase_pipeline_outputs/ambiguous_peaks/get_ambiguous_tracks.sh
@@ -2,9 +2,9 @@
 idr_peaks=$1
 overlap_peaks=$2
 bedtools intersect -v -a $overlap_peaks -b $idr_peaks | cut -f1,2,3 > $overlap_peaks.ambiguous.bed
-cut -f1,2,3 ../../make_pygenometracks/all_three_blacklists.bed >> $overlap_peaks.ambiguous.bed
-bedtools sort -i $overlap_peaks.ambiguous.bed > $overlap_peaks.ambiguous.bed.tmp
+cut -f1,2,3 all_three_blacklists.bed >> $overlap_peaks.ambiguous.bed
+bedtools sort -i $overlap_peaks.ambiguous.bed | bedtools merge -i -  > $overlap_peaks.ambiguous.bed.tmp
 mv $overlap_peaks.ambiguous.bed.tmp $overlap_peaks.ambiguous.bed
-bgzip -f $overlap_peaks.ambiguous.bed
-tabix -p bed $overlap_peaks.ambiguous.bed.gz
+gzip -f $overlap_peaks.ambiguous.bed
+
 
